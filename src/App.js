f8504e6063a5eaf9bcf44d0178114062c1663cfa
Josh Herrera <joshherrera09@gmail.com>
2020-11-17 21:05:52 -0800

    login and home page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Wrapper from "./components/Wrapper/Wrapper";
+import Login from "./components/pages/Login"
+import Home from "./components/pages/Home"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div>
+      <Wrapper>
+      <Route exact path ="/" component={Login} />
+        <Route exact path ="/Login" component={Login} />
+        <Route exact path ="/Home" component={Home} />
+      </Wrapper>
     </div>
-  );
+  </Router>
+    )
 }
 
 export default App;
+
+// /components/pages/Home.js
