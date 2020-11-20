476d738b6f2c23f39bac8358e8ddf2472774b2b9
Marc Anthony Surio <suriomanthony@gmail.com>
2020-11-19 18:21:32 -0800

    import 'bootstrap/dist/css/bootstrap.min.css';

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper/Wrapper";
 import Login from "./components/pages/Login"
 import Home from "./components/pages/Home"
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
