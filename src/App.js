1d1c11426e55757910b5ace022aa24afe75e4a9b
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-12-01 19:51:33 -0800

    newest files Tues 751

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import React,{ useState } from "react";
+import './App.css';
+import Header from './components/Header/Header';
+import TinderCards from './components/TinderCards/TinderCards';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SwipeButtons from './components/SwipeButton/SwipeButtons';
+import Login from './components/Login/Login';
+import ChatHeader from './components/ChatHeader/ChatHeader';
+import Chats from './components/Chats/Chats';
+
+
+
 
 function App() {
+  const [username] = useState('');
+  const [userage] = useState('');
+  const [useractivites] = useState('');
+  const [userpicture] = useState('');
+  
+
+  // function handleClick() {
+  //   debugger;
+  //   let fullname = document.querySelector('#fullName').value,
+  //   age = document.querySelector('#age').value,
+  //   activities= document.querySelector('#activites').value;
+    
+  //   console.log(fullname)
+  //   console.log(age)
+  //   console.log(activities)
+  //   setUser({fullname: fullname, age: age, activities: activities });
+
+  //   history.push('/swipe');
+    
+  // }
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          {/* <Route exact path="/" component={About} /> */}
-          <Route exact path="/discover" component={Discover} />
-          
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <ChatHeader backButton={`/flick?name=${username}&age=${userage}&activities=${useractivites}&picture=${userpicture}`} />
+            <Chats/>
+          </Route>
+          <Route path="/flick">
+            <Header />
+            <TinderCards  />
+            <SwipeButtons />
+          </Route>
+          <Route path="/">
+            <Login   />
+          </Route>
+        </Switch>
+      </Router>
+
+    </div>
   );
 }
 
 export default App;
+
